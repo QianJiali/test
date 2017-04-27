718a757a0a93e3311bf3e4589eaee12822915164
vagrant <vagrant@homestead>
2017-04-27 14:34:44 +0000

    static pages

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'help page')
+@section('content')
+  <h1>帮助页</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'main page')
+@section('contents')
+  <h1>主页</h1>
+@stop
